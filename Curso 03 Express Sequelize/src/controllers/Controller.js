1b2a4fbf3326c3daf3eb2ec990972b28cc1b66ef
Luís Campanholi <lpcampanholi@gmail.com>
2024-08-01 19:55:09 -0300

    crud de pessoas concluído

--- Curso 03 Express Sequelize/src/controllers/Controller.js
+++ Curso 03 Express Sequelize/src/controllers/Controller.js
@@ -1,5 +1,3 @@
-
-
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
@@ -12,6 +10,51 @@ class Controller {
     } catch (erro) {
       // erro
     }
+  };
+
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      // erro
+    };
+  };
+
+  async criaNovo(req, res) {
+    const dadosParaCriacao = req.body;
+    try {
+      const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+      return res.status(200).json(novoRegistroCriado);
+    } catch (erro) {
+      // erro
+    };
+  };
+
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      // isUpdated
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      if (!foiAtualizado) {
+        return res.status(400).json( {message: "Registro não foi atualizado"});
+      };
+      return res.status(200).json( {message: "Atualizado com sucesso"} )
+    } catch (erro) {
+      // erro
+    };
+  };
+
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.excluiRegistro(Number(id));
+      return res.status(200).json({ message: `Id ${id} deletado`})
+    } catch (erro) {
+      return res.status(500).json(erro.message);
+    };
   };
 };
 
